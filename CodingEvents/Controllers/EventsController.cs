26b4d66a8ecedc724a59b8034b4053b32f9520b9
Jose g hernandez <jose_ghj@hotmail.com>
2023-03-29 12:55:18 -0500

    added contacamail

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
+using CodingEvents.ViewModels;
+
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,30 +14,75 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string, string> Events = new Dictionary<string, string>();
-
+     
         // GET: /<controller>/
         public IActionResult Index()
         {
-            ViewBag.events = Events;
-
-            return View();
+            //ViewBag.events = EventData.GetAll();
+            List<Event> events = new List<Event>(EventData.GetAll());
+            return View(events);
         }
 
         [HttpGet]
         public IActionResult Add()
         {
-            return View();
+            AddEventViewModel addEventViewModel = new AddEventViewModel();
+
+            return View(addEventViewModel);
         }
 
         [HttpPost]
-        [Route("/Events/Add")]
-        public IActionResult NewEvent(string name, string desc = "")
+        public IActionResult Add(AddEventViewModel addEventViewModel)
         {
-            Events.Add(name, desc);
+            if (ModelState.IsValid)
+            {
+                Event newEvent = new Event
+                {
+                    Name = addEventViewModel.Name,
+                    Description = addEventViewModel.Description,
+                    ContactEmail = addEventViewModel.ContactEmail,
+                };
+                EventData.Add(newEvent);
+
+                return Redirect("/Events");
+            }
+            return View(addEventViewModel);
+        }
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach (int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
 
             return Redirect("/Events");
         }
+       
+        //[HttpGet("/Events/Edit/{eventId}")]
+        //public IActionResult Edit(int eventId)
+        //{
+        //    Event evtEdit = EventData.GetById(eventId);
+        //    ViewBag.editEvt = evtEdit;
+        //    ViewBag.title = $"Edit Event {evtEdit.Name} (id={evtEdit.Id})";
+        //    return View();
+        //}
+
+        //[HttpPost("/Events/Edit")]
+        //public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        //{
+        //    Event evtEdit = EventData.GetById(eventId);
+        //    evtEdit.Name = name;
+        //    evtEdit.Description = description;
+
+        //    return Redirect("/Events");
+        //}
     }
 }
 
---
+++ CodingEvents/ViewModels/AddEventViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodingEvents.ViewModels
+{
+    public class AddEventViewModel
+    {
+        [Required(ErrorMessage = "Name of event is required.")]
+        [StringLength(50,MinimumLength =3,ErrorMessage ="Even name should be between 3-50 characters.")]
+        public string? Name { get; set; }
+        [Required(ErrorMessage = "Decription of event is required.")]
+        [StringLength(500,ErrorMessage ="Discription over 500 chara.")]
+        public string? Description { get; set; }
+        [EmailAddress]
+        public string? ContactEmail { get; set; }
+    }
+}
